5ce75ac72e882708436e89ee07f0ca2573b7208e
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-05-28 16:56:17 -0700

    🍱 Add icons

---
+++ src/assets/icons/Back.tsx
@@ -0,0 +1,8 @@
+export function Back (): JSX.Element {
+  return (
+    <svg fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' className='size-6'>
+      <path stroke-linecap='round' stroke-linejoin='round' d='M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18' />
+    </svg>
+
+  )
+}
