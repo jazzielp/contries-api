e418c6ec7f61ab4490de511feb691b66a48dcdcf
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-05-28 16:27:41 -0700

    🍱 Add the nunito sans  font

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import '@fontsource-variable/nunito-sans'
 import './index.css'
 
 const rootElement = document.getElementById('root')
