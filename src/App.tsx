34e4dd9ec8c6a3e2f39f44999e66d9d2d929643a
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-05-28 16:20:49 -0700

    💄 Configure Tailwind CSS in the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App (): JSX.Element {
   return (
     <>
-      <h1>Hello World</h1>
+      <h1 className='text-3xl font-bold underline text-red-400'>
+        Hello!
+      </h1>
     </>
   )
 }
