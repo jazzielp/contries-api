697f656aa524e3db7f40e59489a684420a6d67a5
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-09-05 09:35:13 -0700

    ✨ Add error page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { Header } from '@/components/Header'
 import { Toolbar } from '@/components/Toolbar'
 import { ListCountry } from '@/components/ListCountry'
-import { CountryDetail } from './components/CountryDetail'
+import { CountryDetail } from '@/components/CountryDetail'
+import { ErrorPage } from '@/components/ErrorPage'
 import { Routes, Route } from 'react-router'
 function App (): JSX.Element {
   return (
@@ -11,6 +12,7 @@ function App (): JSX.Element {
       <Routes>
         <Route path='/' element={<ListCountry />} />
         <Route path='/country/:name' element={<CountryDetail />} />
+        <Route path='/*' element={<ErrorPage />} />
       </Routes>
     </>
   )
