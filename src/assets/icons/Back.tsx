ba3a30d64f558178f97175752108d0fa519af323
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-09-07 12:25:02 -0700

    ✏️ Switch frmo kebab-case to camelCase

--- src/assets/icons/Back.tsx
+++ src/assets/icons/Back.tsx
@@ -1,7 +1,7 @@
 export function Back (): JSX.Element {
   return (
-    <svg fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' className='size-6'>
-      <path stroke-linecap='round' stroke-linejoin='round' d='M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18' />
+    <svg fill='none' viewBox='0 0 24 24' strokeWidth='1.5' stroke='currentColor' className='size-6'>
+      <path strokeLinecap='round' strokeLinejoin='round' d='M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18' />
     </svg>
 
   )
